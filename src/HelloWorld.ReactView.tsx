4ef59cdfe10bb73dc39414ea53c69e27857ce54c
Dan Fincher <danfincher@microsoft.com>
2024-10-09 11:28:22 -0400

    working

--- src/HelloWorld.ReactView.tsx
+++ src/HelloWorld.ReactView.tsx
@@ -6,17 +6,19 @@ import { getEnvironmentValue } from "@microsoft/azureportal-reactview/Environmen
 
 Az.setTitle(HelloWorldStrings.HelloWorldTitle);
 
+// These lazy loads should be outside of the component....
+
+// This is testing fallback behavior. To test in browser, block the request to load myRemote1 (remoteTest1)  
+// Remote is defined in * so its available in all environments
+const SharedComponent1 = React.lazy(() => import("myRemote1/SharedComponent1").then(module => ({ default: module?.SharedComponent1 ?? (() => <div>fallback module loaded</div>) })));
+
+// Remote is only defined in df, so this is only available in df. Therefore, this no-ops in all other environments
+const SharedComponent2 = React.lazy(() => import("myRemote2/SharedComponent2").then(module => ({ default: module?.SharedComponent2 ?? (() => <div>Module no-oped because this module is not defined in this environment</div>) })));
+
+// // Defined specifically as null in Mooncake. Otherwise, all other environments is implicitly null
+const SharedComponent3 = React.lazy(() => import("myRemote3/SharedComponent3").then(module => ({ default: module?.SharedComponent3 ?? (() => <div>No module to load in this environment</div>) })));
+
 const HelloWorld = () => {
-
-  // This is testing fallback behavior. To test in browser, block the request to load myRemote1 (remoteTest1)  
-  // Remote is defined in * so its available in all environments
-  const SharedComponent1 = React.lazy(() => import("myRemote1/SharedComponent1").then(module => ({ default: module?.SharedComponent1 ?? (() => <div>fallback module loaded</div>) })));
-
-  // Remote is only defined in df, so this is only available in df. Therefore, this no-ops in all other environments
-  const SharedComponent2 = React.lazy(() => import("myRemote2/SharedComponent2").then(module => ({ default: module?.SharedComponent2 ?? (() => <div>Module no-oped because this module is not defined in this environment</div>) })));
-
-  // Defined specifically as null in Mooncake. Otherwise, all other environments is implicitly null
-  const SharedComponent3 = React.lazy(() => import("myRemote3/SharedComponent3").then(module => ({ default: module?.SharedComponent3 ?? (() => <div>No module to load in this environment</div>) })));
 
   React.useEffect(() => {
     console.log("env", getEnvironmentValue("moduleMappings"));
